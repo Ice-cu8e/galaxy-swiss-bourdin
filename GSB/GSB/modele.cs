67059007ebcb1dcf49daf3cc52751c738e34999d
SIO <SIO@ST211>
2017-09-25 17:15:49 +0200

    modele.cs

---
+++ GSB/GSB/modele.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB
+{
+    public static class modele
+    {
+        private static string Visiteur;
+
+        public static string Visiteur1
+        {
+            get
+            {
+                return Visiteur;
+            }
+
+            set
+            {
+                Visiteur = value;
+            }
+        }
+    }
+}
