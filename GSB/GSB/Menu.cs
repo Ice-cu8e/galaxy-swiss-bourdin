048a53b5b55587055c0ec7b973fee422ae9a4969
Cloarec Yann <cloarecyann@gmail.com>
2017-09-27 19:31:11 +0200

    Début

--- GSB/GSB/Menu.cs
+++ GSB/GSB/Menu.cs
@@ -10,12 +10,11 @@ using System.Windows.Forms;
 
 namespace GSB
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
-
     }
 }
